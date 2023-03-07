1ff5ba5eec77456370e9ae2a9df1adec9ed57c8f
Shepard <koala_butcher14@mailbox.org>
2023-03-07 11:09:58 +0100

    reworked partial mapping, streamlined variable names a bit, gave files proper extensions, added all skills

--- droplog_mtr.ps1
+++ droplog_mtr.ps1
@@ -1,19 +1,14 @@
-﻿Get-Content "$PSScriptRoot\config.ini" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+﻿$ignoreItems = @()
+$magicExceptions = @('銳利之斧')
+$nameMap = @{"超強的" = "Superior";}
+$dictSource = "https://gist.githubusercontent.com/r0-se/4f72452ac805fde9ff018df81867b49d/raw"
+$dictFile = "twdict.txt"
 
-$Path = $h.Get_Item("savepath")
-$hookurl = $h.Get_Item("hookurl")
-
-$IgnoreItems = @()
-$magic_exceptions= @('銳利之斧')
-
-$dict_source = "https://gist.githubusercontent.com/r0-se/4f72452ac805fde9ff018df81867b49d/raw"
-$dict_file = "twdict.txt"
-
-$RuneList=@{
+$runeList=@{
     "El" = 1;"Eld" = 2;"Tir" = 3;"Nef" = 4;"Eth" = 5;"Ith" = 6;"Tal" = 7;"Ral" = 8;"Ort" = 9; "Thul" = 10;"Amn" = 11;"Sol" = 12;"Shael" = 13;"Dol" = 14;"Hel" = 15;"Io" = 16; "Lum" = 17;"Ko" = 18;"Fal" = 19;"Lem" = 20;"Pul" = 21;"Um" = 22;"Mal" = 23;"Ist" = 24;"Gul" = 25;"Vex" = 26;"Ohm" = 27;"Lo" = 28; "Sur" = 29;"Ber" = 30;"Jah" = 31;"Cham" = 32;"Zod" = 33;
 }
 
-$DiscColor=@{
+$discColor=@{
     "white" = 16777215;
     "gold" = 13351039; 
     "green" = 65280;
@@ -22,13 +17,20 @@ $DiscColor=@{
     "orange" = 15628803;
     "grey" = 5592405;
 }
+
+#loading config file
+Get-Content "$PSScriptRoot\config.ini" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+$path = $h.Get_Item("savepath")
+$hookUrl = $h.Get_Item("hookurl")#
+$minRune = If ($RuneList.Contains($h.Get_Item("min_rune"))) {$RuneList[$h.Get_Item("min_rune")]} Else {0}
+
 function Get-rare-item {
     param([string]$name)
     $splitted = $name.split(" ")
 
     if($splitted.Count -eq 2) {
-        if ($prefix_rare.cn.Contains($splitted[0]) -and $suffix_rare.cn.Contains($splitted[1])) {
-            return ($prefix_rare | Where-Object CN -eq $splitted[0]).en + " " + ($suffix_rare | Where-Object CN -eq $splitted[1]).en
+        if ($prefixRare.cn.Contains($splitted[0]) -and $suffixRare.cn.Contains($splitted[1])) {
+            return ($prefixRare | Where-Object CN -eq $splitted[0]).en + " " + ($suffixRare | Where-Object CN -eq $splitted[1]).en
         }
     }
     return $false
@@ -40,21 +42,22 @@ function Get-magic-item {
     
     if($splitted.Count -eq 1) {
         #stupid workaround for itemnames which match <prefix><type> but are actually white
-        if($magic_exceptions.Contains($name)){
+        if($magicExceptions.Contains($name)){
             break
         }
 
         #this is for names in the form "<prefix><type>" - NOT separated by a space
         #example 魚叉手之超大型護身符 harpoonists(魚叉手之) grand charm(超大型護身符)
-        foreach($prefix in $prefix_magic){
+        foreach($prefix in $prefixMagic){
             if ($name.StartsWith($prefix.cn)) {
                 $splitted = $name.insert($prefix.cn.length, ' ').split(" ")
                 break
             }
         }
 
+        #this is for names in the form "<type><suffix>" - NOT separated by a space
         #example 小護身符幸運 small charm (小護身符) of luck(幸運)
-        foreach($suffix in $suffix_magic){
+        foreach($suffix in $suffixMagic){
             if ($name.EndsWith($suffix.cn)) {
                 #also turns around the order so this is processed by the translation later
                 $splitted = ($suffix.cn, $name.Substring(0,$name.length-$suffix.cn.length))
@@ -65,16 +68,16 @@ function Get-magic-item {
 
     if($splitted.Count -eq 2) {
         #this is for names in the form "<prefix> <type>" - separated by a space
-        if ($prefix_magic.cn.Contains($splitted[0])) {
+        if ($prefixMagic.cn.Contains($splitted[0])) {
             $type = Get-Name-Translation -original $splitted[1]
-            $current_prefix = ($prefix_magic | Where-Object cn -eq $splitted[0] | Select-Object -Last 1).en
+            $current_prefix = ($prefixMagic | Where-Object cn -eq $splitted[0] | Select-Object -Last 1).en
             return "$current_prefix $type"
         }
 
         #this is for names in the form "<suffix> <type>" - separated by a space -> not sure if that even happens naturally anymore
-        if ($suffix_magic.cn.Contains($splitted[0])) {
+        if ($suffixMagic.cn.Contains($splitted[0])) {
             $type = Get-Name-Translation -original $splitted[1]
-            $current_suffix = ($suffix_magic | Where-Object cn -eq $splitted[0]| Select-Object -Last 1).en 
+            $current_suffix = ($suffixMagic | Where-Object cn -eq $splitted[0]| Select-Object -Last 1).en 
             return "$type $current_suffix"
         }
     }
@@ -82,10 +85,10 @@ function Get-magic-item {
     #this is for names in the form "<suffix> <suffix> <type>" - separated by a space; this is weird yo
     #example 火光之 品質的 超大型護身符 sparking(火光之) of quality(品質的) grand charm(超大型護身符)
     if($splitted.Count -eq 3) {
-        if ($prefix_magic.cn.Contains($splitted[0]) -and $suffix_magic.cn.Contains($splitted[1])) {
+        if ($prefixMagic.cn.Contains($splitted[0]) -and $suffixMagic.cn.Contains($splitted[1])) {
             $type = Get-Name-Translation -original $splitted[2]
-            $current_prefix = ($prefix_magic | Where-Object cn -eq $splitted[0] | Select-Object -Last 1).en
-            $current_suffix = ($suffix_magic | Where-Object cn -eq $splitted[1] | Select-Object -Last 1).en 
+            $current_prefix = ($prefixMagic | Where-Object cn -eq $splitted[0] | Select-Object -Last 1).en
+            $current_suffix = ($suffixMagic | Where-Object cn -eq $splitted[1] | Select-Object -Last 1).en 
             return "$current_prefix $type $current_suffix"
         }
     }   
@@ -98,24 +101,28 @@ function Get-Mod-Translation {
         return $original
     }
 
+    #remove all of those horrible brackets
+    $original = $original.replace('（','(').replace('）',')')
+
+    foreach ($s in $regexMaps) {
+        if ($original -match $s.cn) {
+            $result = $s.en
+            for($i=1;$i -lt $Matches.count;$i++) {
+                $result = $result -replace "\{$($i-1)\}", $Matches[$i]
+            }
+            return Get-Mod-Translation -original $result
+        }
+    }
+
     #this is supposed to insert skill and class names or weapon speeds
-    foreach ($s in $staticmaps) {
+    foreach ($s in $staticMaps) {
         if ($original.Contains($s.cn)) {
             $pre = Get-Name-Translation -original $original.Substring(0, $original.IndexOf($s.cn)).Trim()
             $post = Get-Name-Translation -original $original.Substring($original.IndexOf($s.cn)+($s.cn.length)).Trim()
             return Get-Mod-Translation -original "$pre $($s.en) $post".Trim()
         }
     }
-    
-    foreach ($s in $regexmaps) {
-        if ($original -match $s.cn) {
-            $result = $s.en
-            for($i=1;$i -lt $Matches.count;$i++) {
-                $result = $result -replace "\{$($i-1)\}", $Matches[$i]
-            }
-            return $result
-        }
-    }
+
     return "not in dict: $original"
 }
 
@@ -124,11 +131,11 @@ function Get-Name-Translation {
     if ($original -eq '') {
         return ''
     }
-    foreach ($s in $staticmaps) {
-        if ($original.Contains($s.cn)) {
-            $pre = Get-Name-Translation -original $original.Substring(0, $original.IndexOf($s.cn)).Trim()
-            $post = Get-Name-Translation -original $original.Substring($original.IndexOf($s.cn)+($s.cn.length)).Trim()
-            return "$pre $($s.en) $post".Trim()
+    foreach ($s in $nameMap.Keys) {
+        if ($original.Contains($s)) {
+            $pre = Get-Name-Translation -original $original.Substring(0, $original.IndexOf($s)).Trim()
+            $post = Get-Name-Translation -original $original.Substring($original.IndexOf($s)+($s.length)).Trim()
+            return "$pre $($nameMap.$s) $post".Trim()
         }
     }
     $out = $tw_dict |  Where-Object { $_."tw-old" -eq $original } | Select-Object -Last 1
@@ -152,15 +159,20 @@ function itemintoobj {
     }
     $item = $item -replace '\x00', ''
     
+    #timestamp - easy
     if ($item -match '(?<first>[0-9]{4}\/[0-9]{2}\/[0-9]{2}\s[0-9]{2}:[0-9]{2})') {
         $returnItem.Timestamp = $matches[0]
     }
 
+    #heres the name
     if ($item -match '.*(?=\d{4}\/)') {
         $returnItem.Name = $matches[0]
+        #if it starts with rune - make it orange
         if($returnItem.Name.StartsWith("符文：")) {
             $returnItem.Color = $DiscColor.orange
         }
+
+        #find out if its magic by looking for prefix/suffix
         $magic = Get-magic-item -name $returnItem.Name
         if ($magic) {
             $returnItem.Color = $DiscColor.blue
@@ -170,12 +182,12 @@ function itemintoobj {
         }
     }
 
+    #here we get the mods of the item
     if ($item -match '(?<=\d{4}\/\d{2}\/\d{2} \d{2}:\d{2})[\s\S]*') {
         $returnItem.Mods = New-Object System.Collections.ArrayList(,$matches[0].split("`n"))
         $returnItem.Mods.Reverse()
         
-        #this happens for set items and maybe uniques (e.g. Tal Rashas Education - Amulet)
-        #happens also for rare.. maybe blue
+        #yellow//green//gold items seem to have their full name to be equal to first and second mod
         # $returnItem.Mods[0] <-- name
         # $returnItem.Mods[1] <-- type
         if ($returnItem.Name -eq $returnItem.Mods[1]+$returnItem.Mods[0]){
@@ -201,18 +213,18 @@ function itemintoobj {
             }
 
             #handle the first line of mods
-            if($i -eq 0) {
-                #non named items (white blue yellow grey) have def or damage in the first line
-                if (($returnItem.mods[$i].StartsWith("雙手傷害：") -or $returnItem.mods[$i].StartsWith("防禦：") -or $returnItem.mods[$i].StartsWith("單手傷害："))){
-                    if ($returnItem.color -ne $DiscColor.blue -and $returnItem.color -ne $DiscColor.blue){
-                        $returnItem.Color = $DiscColor.white
-                    }
+            if($i -eq 0 -and $returnItem.color -ne $DiscColor.blue) {
+                #non named items (white/grey) have def or damage in the first line
+                if (($returnItem.mods[$i].StartsWith("雙手傷害：") -or 
+                    $returnItem.mods[$i].StartsWith("防禦：") -or 
+                    $returnItem.mods[$i].StartsWith("單手傷害：")))
+                {
+                    $returnItem.Color = $DiscColor.white
                     $returnItem.ModsEN.Add((Get-Mod-Translation -original $returnItem.mods[$i]))
                 } else {
-                    #colored item have itemtype as first attribute -> translate as name
+                    #colored items (yellow/green/gold) have itemtype as first attribute -> translate as name
                     $returnItem.ModsEN.Add((Get-Name-Translation -original $returnItem.mods[$i]))
                 }
-                
             } else {
                 #normal mod
                 $returnItem.ModsEN.Add((Get-Mod-Translation -original $returnItem.mods[$i]))
@@ -261,8 +273,8 @@ function Watch-Folder {
         [string]$path
     )
 
-    $currentsize = Get-ChildItem -path $path -Filter "入庫記錄.txt" -Recurse | Select-Object Length, PsPath
-    if ($null -eq $currentsize) { 
+    $currentSize = Get-ChildItem -path $path -Filter "入庫記錄.txt" -Recurse | Select-Object Length, PsPath
+    if ($null -eq $currentSize) { 
         Write-Warning "no droplogs found in $path -> terminating"
         Exit -1    
     }
@@ -293,20 +305,18 @@ function Watch-Folder {
             if ($compare -ne $($o.ItemCount)) { 
                 (Get-Process -pid $PID).MainWindowHandle | out-null
                 
-                $AmountofNewItems = $compare - $($o.ItemCount)
-                $loopervar = $($o.ItemCount)
+                $amountOfNewItems = $compare - $($o.ItemCount)
+                $looperVar = $($o.ItemCount)
                 $o.ItemCount = (get-itemcount -Path $o.Path)
-                #regex map refresh here while workingon it so that dont have to restart script for changes
-                $global:regexmaps = Import-Csv $PSScriptRoot\regex.txt -Delimiter ";" -Header cn,en
-                write-verbose -message "$($o.name) has changed with $AmountofNewItems var is $loopervar and $compare"
-                for ($loopervar; $loopervar -lt $compare; $loopervar++) {
-                    Write-verbose -Message "lets get $($o.Path) -Index $loopervar"
-                    $item = Get-Item -Path $($o.Path) -Index $loopervar
-                    if ($IgnoreItems.Contains($item.nameEN) -or 
+                write-verbose -message "$($o.name) has changed with $amountOfNewItems var is $loopervar and $compare"
+                for ($looperVar; $looperVar -lt $compare; $looperVar++) {
+                    Write-verbose -Message "lets get $($o.Path) -Index $looperVar"
+                    $item = Get-Item -Path $($o.Path) -Index $looperVar
+                    if ($ignoreItems.Contains($item.nameEN) -or 
                         (
                             $item.nameEN.EndsWith(" Rune") -and 
                             $RuneList.Contains($item.nameEN.split(" ")[0]) -and
-                            $RuneList[$item.nameEN.split(" ")[0]] -le 19
+                            $RuneList[$item.nameEN.split(" ")[0]] -lt $minRune
                         )
                     ) { 
                         write-host -message "ignoring $($item.nameEN)" 
@@ -338,7 +348,7 @@ function sendToWebhook {
         [PSCustomObject]$item,
         [string]$name
     )
-    #for privacy reasons
+    #for privacy reasons remove domain
     $name = $name.split("@")[0]
     $description = ""
     $title += "$($item.nameEN)`n"
@@ -372,10 +382,10 @@ function helper {
         return "This powershell version is too old! You need 7+" 
     }
 
-    if ($null -eq (Get-ChildItem $PSScriptRoot\$dict_file -ErrorAction SilentlyContinue)) {
+    if ($null -eq (Get-ChildItem "$PSScriptRoot\$dictFile" -ErrorAction SilentlyContinue)) {
         Write-Warning "The translation file does not exist -> trying to download"
         try {
-            invoke-RestMethod $dict_source -OutFile $dict_file
+            invoke-RestMethod $dictSource -OutFile $dictFile
         }
         catch {
             Write-Warning "Download seems to have failed -> terminating"
@@ -384,12 +394,12 @@ function helper {
     }
 }
 
-$tw_dict = Get-Content $PSScriptRoot\twdict.txt -raw | ConvertFrom-Json
-$global:staticmaps = Import-Csv $PSScriptRoot\static-name.txt -Header cn,en
-$global:regexmaps = Import-Csv $PSScriptRoot\regex.txt -Delimiter ";" -Header cn,en
-$global:prefix_rare = Import-Csv $PSScriptRoot\prefix_rare.txt -Header cn,en 
-$global:suffix_rare = Import-Csv $PSScriptRoot\suffix_rare.txt -Header cn,en 
-$global:prefix_magic = Import-Csv $PSScriptRoot\prefix_magic.txt -Header cn,en 
-$global:suffix_magic = Import-Csv $PSScriptRoot\suffix_magic.txt -Header cn,en 
+$global:twDict = Get-Content "$PSScriptRoot\data\twdict.json" -raw | ConvertFrom-Json
+$global:staticMaps = Import-Csv "$PSScriptRoot\data\static_mapping.csv" -Header cn,en
+$global:regexMaps = Import-Csv "$PSScriptRoot\data\regex.csv" -Delimiter ";" -Header cn,en
+$global:prefixRare = Import-Csv "$PSScriptRoot\data\prefix_rare.csv" -Header cn,en 
+$global:suffixRare = Import-Csv "$PSScriptRoot\data\suffix_rare.csv" -Header cn,en 
+$global:prefixMagic = Import-Csv "$PSScriptRoot\data\prefix_magic.csv" -Header cn,en 
+$global:suffixMagic = Import-Csv "$PSScriptRoot\data\suffix_magic.csv" -Header cn,en 
 helper
 Watch-Folder $path
