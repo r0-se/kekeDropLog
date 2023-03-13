0c9adb3d787eac082f5335f6b3205a1869c20f17
Shepard <koala_butcher14@mailbox.org>
2023-03-13 22:15:10 +0100

    changed another >  for readability

--- module/helper.ps1
+++ module/helper.ps1
@@ -39,7 +39,7 @@ function New-Config {
 
 function Get-Config {
     if (-not (Test-Path "$PSScriptRoot\..\config.ini")) {
-        $null = New-Config
+        New-Config > $null
     }
     $h = @{}
     Get-Content "$PSScriptRoot\..\config.ini" | foreach-object -process { $k = [regex]::split($_, '='); if (($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
